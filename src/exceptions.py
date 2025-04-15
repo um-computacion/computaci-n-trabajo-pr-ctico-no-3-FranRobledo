342d63845879bd17b37c6b7a95a6d14e1bb3fc9e
FranRobledo <f.robledo@alumno.um.edu.ar>
2025-04-15 20:14:56 -0300

    Implementación de excepciones y validación

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,14 +1,12 @@
 class NumeroDebeSerPositivo(Exception):
     """Excepción lanzada cuando se ingresa un número negativo."""
-    
-    
 
 def ingrese_numero():
-    entrada = input("Ingrese un número: ")
-    try:
-        numero = int(entrada)  
+    entrada = input ("Ingrese un numero: ")
+    try: 
+        numero = int(entrada)
         if numero < 0:
-            raise NumeroDebeSerPositivo("La entrada debe ser un numero positivo")  
+            raise NumeroDebeSerPositivo("El numero debe ser positivo. \n")
         return numero
     except ValueError:
-        raise ValueError("La entrada debe ser un número válido") 
+        raise ValueError("La entrada debe ser un numero válido. \n")
