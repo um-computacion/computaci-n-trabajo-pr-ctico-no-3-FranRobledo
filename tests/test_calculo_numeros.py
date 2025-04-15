7506bf56cc9f56ef585246c7162c660d8d3efcdf
FranRobledo <f.robledo@alumno.um.edu.ar>
2025-04-15 19:21:20 -0300

    Implementación de pruebas para validación de números

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -1,8 +1,5 @@
 import unittest
-from src.exceptions import (
-    ingrese_numero,
-    NumeroDebeSerPositivo,
-)
+from src.exceptions import (ingrese_numero,NumeroDebeSerPositivo,)
 from unittest.mock import patch
 
 class TestCalculoNumeros(unittest.TestCase):
@@ -15,6 +12,11 @@ class TestCalculoNumeros(unittest.TestCase):
     @patch('builtins.input', return_value='-100')
     def test_ingreso_negativo(self, patch_input):
         with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+
+    @patch('builtins.input', return_value='AAA')
+    def test_ingreso_letras(self, patch_input):
+        with self.assertRaises(ValueError):
             ingrese_numero()
 
 if __name__ == '__main__':
